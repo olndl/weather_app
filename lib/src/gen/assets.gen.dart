0397c440e8f357ee2f6a95e46ca022f709cba7c5
Olga Shiryavceva <olundel@yandex-team.ru>
2023-02-14 20:54:22 +0300

    code style

---
+++ lib/src/gen/assets.gen.dart
@@ -0,0 +1,315 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+import 'package:flutter/services.dart';
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class $LibGen {
+  const $LibGen();
+
+  $LibSrcGen get src => const $LibSrcGen();
+}
+
+class $LibSrcGen {
+  const $LibSrcGen();
+
+  $LibSrcAssetsGen get assets => const $LibSrcAssetsGen();
+}
+
+class $LibSrcAssetsGen {
+  const $LibSrcAssetsGen();
+
+  $LibSrcAssetsPngGen get png => const $LibSrcAssetsPngGen();
+
+  $LibSrcAssetsSvgGen get svg => const $LibSrcAssetsSvgGen();
+}
+
+class $LibSrcAssetsPngGen {
+  const $LibSrcAssetsPngGen();
+
+  /// File path: lib/src/assets/png/background.png
+  AssetGenImage get background =>
+      const AssetGenImage('lib/src/assets/png/background.png');
+
+  /// File path: lib/src/assets/png/clear_night.png
+  AssetGenImage get clearNight =>
+      const AssetGenImage('lib/src/assets/png/clear_night.png');
+
+  /// File path: lib/src/assets/png/day_clear.png
+  AssetGenImage get dayClear =>
+      const AssetGenImage('lib/src/assets/png/day_clear.png');
+
+  /// File path: lib/src/assets/png/day_clouds.png
+  AssetGenImage get dayClouds =>
+      const AssetGenImage('lib/src/assets/png/day_clouds.png');
+
+  /// File path: lib/src/assets/png/day_fog.png
+  AssetGenImage get dayFog =>
+      const AssetGenImage('lib/src/assets/png/day_fog.png');
+
+  /// File path: lib/src/assets/png/day_rain.png
+  AssetGenImage get dayRain =>
+      const AssetGenImage('lib/src/assets/png/day_rain.png');
+
+  /// File path: lib/src/assets/png/day_snow.png
+  AssetGenImage get daySnow =>
+      const AssetGenImage('lib/src/assets/png/day_snow.png');
+
+  /// File path: lib/src/assets/png/day_storm.png
+  AssetGenImage get dayStorm =>
+      const AssetGenImage('lib/src/assets/png/day_storm.png');
+
+  /// File path: lib/src/assets/png/night _fog.png
+  AssetGenImage get nightFog =>
+      const AssetGenImage('lib/src/assets/png/night _fog.png');
+
+  /// File path: lib/src/assets/png/night_clouds.png
+  AssetGenImage get nightClouds =>
+      const AssetGenImage('lib/src/assets/png/night_clouds.png');
+
+  /// File path: lib/src/assets/png/night_rain.png
+  AssetGenImage get nightRain =>
+      const AssetGenImage('lib/src/assets/png/night_rain.png');
+
+  /// File path: lib/src/assets/png/night_snow.png
+  AssetGenImage get nightSnow =>
+      const AssetGenImage('lib/src/assets/png/night_snow.png');
+
+  /// File path: lib/src/assets/png/night_storm.png
+  AssetGenImage get nightStorm =>
+      const AssetGenImage('lib/src/assets/png/night_storm.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        background,
+        clearNight,
+        dayClear,
+        dayClouds,
+        dayFog,
+        dayRain,
+        daySnow,
+        dayStorm,
+        nightFog,
+        nightClouds,
+        nightRain,
+        nightSnow,
+        nightStorm
+      ];
+}
+
+class $LibSrcAssetsSvgGen {
+  const $LibSrcAssetsSvgGen();
+
+  /// File path: lib/src/assets/svg/addition.svg
+  SvgGenImage get addition =>
+      const SvgGenImage('lib/src/assets/svg/addition.svg');
+
+  /// File path: lib/src/assets/svg/air.svg
+  SvgGenImage get air => const SvgGenImage('lib/src/assets/svg/air.svg');
+
+  /// File path: lib/src/assets/svg/arrow-down.svg
+  SvgGenImage get arrowDown =>
+      const SvgGenImage('lib/src/assets/svg/arrow-down.svg');
+
+  /// File path: lib/src/assets/svg/arrow_up.svg
+  SvgGenImage get arrowUp =>
+      const SvgGenImage('lib/src/assets/svg/arrow_up.svg');
+
+  /// File path: lib/src/assets/svg/empty_state.svg
+  SvgGenImage get emptyState =>
+      const SvgGenImage('lib/src/assets/svg/empty_state.svg');
+
+  /// File path: lib/src/assets/svg/eye.svg
+  SvgGenImage get eye => const SvgGenImage('lib/src/assets/svg/eye.svg');
+
+  /// File path: lib/src/assets/svg/eye_image.svg
+  SvgGenImage get eyeImage =>
+      const SvgGenImage('lib/src/assets/svg/eye_image.svg');
+
+  /// File path: lib/src/assets/svg/fog.svg
+  SvgGenImage get fog => const SvgGenImage('lib/src/assets/svg/fog.svg');
+
+  /// File path: lib/src/assets/svg/house.svg
+  SvgGenImage get house => const SvgGenImage('lib/src/assets/svg/house.svg');
+
+  /// File path: lib/src/assets/svg/hover.svg
+  SvgGenImage get hover => const SvgGenImage('lib/src/assets/svg/hover.svg');
+
+  /// File path: lib/src/assets/svg/humidity.svg
+  SvgGenImage get humidity =>
+      const SvgGenImage('lib/src/assets/svg/humidity.svg');
+
+  /// File path: lib/src/assets/svg/magnifyingglass.svg
+  SvgGenImage get magnifyingglass =>
+      const SvgGenImage('lib/src/assets/svg/magnifyingglass.svg');
+
+  /// File path: lib/src/assets/svg/moon_clear.svg
+  SvgGenImage get moonClear =>
+      const SvgGenImage('lib/src/assets/svg/moon_clear.svg');
+
+  /// File path: lib/src/assets/svg/sunrise.svg
+  SvgGenImage get sunrise =>
+      const SvgGenImage('lib/src/assets/svg/sunrise.svg');
+
+  /// File path: lib/src/assets/svg/temp.svg
+  SvgGenImage get temp => const SvgGenImage('lib/src/assets/svg/temp.svg');
+
+  /// File path: lib/src/assets/svg/wind.svg
+  SvgGenImage get wind => const SvgGenImage('lib/src/assets/svg/wind.svg');
+
+  /// File path: lib/src/assets/svg/wind_rose.svg
+  SvgGenImage get windRose =>
+      const SvgGenImage('lib/src/assets/svg/wind_rose.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [
+        addition,
+        air,
+        arrowDown,
+        arrowUp,
+        emptyState,
+        eye,
+        eyeImage,
+        fog,
+        house,
+        hover,
+        humidity,
+        magnifyingglass,
+        moonClear,
+        sunrise,
+        temp,
+        wind,
+        windRose
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $LibGen lib = $LibGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider() => AssetImage(_assetName);
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme theme = const SvgTheme(),
+    //ColorFilter? colorFilter,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    //@deprecated Clip? clipBehavior,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      theme: theme,
+      //colorFilter: colorFilter,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      //clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
